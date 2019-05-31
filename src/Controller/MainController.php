a38f1bbff6403660a0a28a857e2678146e05b1d4
Franco Reynoso <reynoso.franco@gmail.com>
2019-05-31 10:01:12 -0500

    Commit Franco CSS Login

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -14,4 +14,12 @@ class MainController extends AbstractController
     {
         return $this->render('index.html.twig');
     }
+
+    /**
+     * @Route("/registrate", name="singup")
+     */
+    public function singup()
+    {
+    	return $this->render('singup.html.twig');
+    }
 }
